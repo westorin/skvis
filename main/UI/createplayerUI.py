961a8b965ad11a69ebd715b505e8612ff621bd3e
einarberg <59967614+einarberg@users.noreply.github.com>
2025-12-02 20:18:41 +0000

    comment on problem with creating player

--- main/UI/createplayerUI.py
+++ main/UI/createplayerUI.py
@@ -18,6 +18,7 @@ class PlayerUI:
         username = input("Username (handle): ")
         team = input("Team: ")
 
+        # TODO It registers player even if you enter nothing as an input
         try:
             self.pm.register_player(
                 name, dob, address, phone, email, url, username, team
