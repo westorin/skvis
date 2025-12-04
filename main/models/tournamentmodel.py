00635d8a76fa502495a99f20d5efb7acbc93b521
haalldor <halldor.203@gmail.com>
2025-12-04 15:23:57 +0000

    tournament debugged

--- main/models/tournamentmodel.py
+++ main/models/tournamentmodel.py
@@ -1,5 +1,17 @@
 class Tournament:
-    def __init__(self, tournament_id, name, start, end, location, contact_email, contact_phone, teams, matches, winner):
+    def __init__(
+        self,
+        name,
+        start,
+        end,
+        location,
+        contact_email,
+        contact_phone,
+        teams=None,
+        matches=None,
+        winner=None,
+        tournament_id=None,
+    ):
         self.tournament_id = tournament_id
         self.name = name
         self.start = start
@@ -7,6 +19,6 @@ class Tournament:
         self.location = location
         self.contact_email = contact_email
         self.contact_phone = contact_phone
-        self.teams = teams
-        self.matches = matches
+        self.teams = teams or []
+        self.matches = matches or []
         self.winner = winner
