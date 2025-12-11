4090184966171a331d89fd04bd1f8d9f72fcc8d9
Kristján Valur Gíslason <kristjanvgisli@gmail.com>
2025-12-11 17:21:16 +0000

    3 layer structure prep

--- main/wrappers/logicwrapper.py
+++ main/wrappers/logicwrapper.py
@@ -8,18 +8,15 @@ from main.logic.loginmanager import LoginManager
 #from main.logic.clearScreenInTerminal import clear_screen
 
 class LogicWrapper:
-    def __init__(self, data):
-        self.player_manager = PlayerManager(data.players)
-        self.team_manager = TeamManager(data.teams, data.players)
+    def __init__(self, data) -> None:
+        self.player_manager: PlayerManager = PlayerManager(data.players)
+        self.team_manager: TeamManager = TeamManager(data.teams, data.players)
         self.player_manager.team_manager = self.team_manager  # Link TeamManager to PlayerManager
-
-        self.match_manager = MatchManager(data.matches)
-
-        self.tournament_manager = TournamentManager(
+        self.match_manager: MatchManager = MatchManager(data.matches)
+        self.tournament_manager: TournamentManager = TournamentManager(
             data.tournaments,
             data.teams,
             self.match_manager
         )
-
-        #self.search_manager = SearchManager(data.players,data.teams,data.tournaments)
-        self.login_manager = LoginManager(data.players, data.roles, data.passwords)
+        #self.search_manager: SearchManager = SearchManager(data.players, data.teams, data.tournaments)
+        self.login_manager: LoginManager = LoginManager(data.players, data.roles, data.passwords)
