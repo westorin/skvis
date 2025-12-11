25c6d2a8885f0011fc728b90a9f6c53c6f4f12f4
einarberg <59967614+einarberg@users.noreply.github.com>
2025-12-11 12:46:06 +0000

    updated main ui and run py

--- run.py
+++ run.py
@@ -1,32 +1,4 @@
 from main.UI.mainUI import MainUI
-from main.UI.homepage_UI import homepageUI 
-from main.UI.pickTimeOfTournament import PickTimeOfTournamentUI
-from main.UI.teamLeaderBoardUI import TeamLeaderBoardUI
-from main.UI.listOfTeams import ListOfTeamsUI
-from main.UI.listOfClubs import listOfClubsUI
-from main.UI.login import LoginUI
-from main.UI.listOfPlayers import ListOfPlayersUI
-from main.UI.addtournament import AddTournamentUI
-from main.UI.search import SearchUI
-from main.UI.team import TeamUI
-from main.UI.teamAllInfo import TeamAllInfoUI
-from main.UI.pastTournamentsList import PastTournamentsUI
-from main.UI.addTeam import AddTeamUI
-from main.UI.pickLeaderBoard import PickLeaderBoardUI
 
-
-run_program = MainUI(homepageUI, 
-                     PickTimeOfTournamentUI, 
-                     PickLeaderBoardUI, 
-                     ListOfTeamsUI, 
-                     listOfClubsUI, 
-                     LoginUI, 
-                     ListOfPlayersUI, 
-                     AddTournamentUI, 
-                     SearchUI, 
-                     TeamUI, 
-                     TeamAllInfoUI, 
-                     PastTournamentsUI, 
-                     AddTeamUI, 
-                     TeamLeaderBoardUI)
+run_program = MainUI()
 run_program.run()
