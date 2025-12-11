ccdc76a779c9821d14f1f6c3a0e89c23526bd2f4
einarberg <59967614+einarberg@users.noreply.github.com>
2025-12-11 20:29:13 +0000

    added Tournament info

---
+++ main/UI/schedule.py
@@ -0,0 +1,6 @@
+from main.wrappers.datawrapper import DataWrapper
+from main.wrappers.logicwrapper import LogicWrapper
+
+class ScheduleUI():
+    def print_schedule(self, tournament_name):
+        pass
