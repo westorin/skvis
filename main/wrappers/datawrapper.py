5bfc11ef65dacea25064794bbb3a5c7325bf7fe6
haalldor <halldor.203@gmail.com>
2025-12-12 21:59:01 +0000

    DataWrapper and LogicWrapper cleanup

--- main/wrappers/datawrapper.py
+++ main/wrappers/datawrapper.py
@@ -10,11 +10,12 @@ from main.repo.passwordrepo import PasswordRepository
 from main.repo.rolesrepo import RoleRepository
 
 class DataWrapper:
-    def __init__(self):
-        self.players = PlayerRepository()
-        self.teams = TeamRepository()
-        self.tournaments = TournamentRepository()
-        self.matches = MatchRepository()
-        self.clubs = ClubRepository()
-        self.passwords = PasswordRepository()
-        self.roles = RoleRepository()
+    """Holds repository instances (data access layer)."""
+    def __init__(self) -> None:
+        self.players: PlayerRepository = PlayerRepository()
+        self.teams: TeamRepository = TeamRepository()
+        self.tournaments: TournamentRepository = TournamentRepository()
+        self.matches: MatchRepository = MatchRepository()
+        self.clubs: ClubRepository = ClubRepository()
+        self.passwords: PasswordRepository = PasswordRepository()
+        self.roles: RoleRepository = RoleRepository()
