9b6cd51ebbcd973698272cfd74450056ebd18be3
Kristján Valur Gíslason <kristjanvgisli@gmail.com>
2025-12-12 15:15:09 +0000

    performance on its way

---
+++ main/logic/performancemanager.py
@@ -0,0 +1,12 @@
+class PerformanceManager:
+    def __init__(self, logic):
+        self.logic = logic
+
+    def compute_global_team_performance():
+        pass
+
+    def export_performance_to_csv():
+        pass
+
+    
+        
