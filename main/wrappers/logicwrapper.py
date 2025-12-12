9ca48006af2690a2be1140e1199ea70e19d7e943
einarberg <59967614+einarberg@users.noreply.github.com>
2025-12-12 21:55:07 +0000

    clear screen

--- main/wrappers/logicwrapper.py
+++ main/wrappers/logicwrapper.py
@@ -3,14 +3,18 @@ from main.logic.playermanager import PlayerManager
 from main.logic.teammanager import TeamManager
 from main.logic.tournamentmanager import TournamentManager
 from main.logic.matchmanager import MatchManager
-from main.logic.searchmanager import SearchManager
 from main.logic.loginmanager import LoginManager
 #from main.logic.leaderboardmanager import LeaderboardManager
-#from main.logic.clearScreenInTerminal import clear_screen
+from main.logic.clearScreenInTerminal import ClearScreenLogic
+from main.logic.leaderboardmanager import LeaderboardManager
+
+
+
 
 class LogicWrapper:
     def __init__(self, data) -> None:
         self.data = data
+        self.clear_screen: ClearScreenLogic = ClearScreenLogic() 
         self.player_manager: PlayerManager = PlayerManager(data.players)
         self.team_manager: TeamManager = TeamManager(data.teams, data.players, data.passwords)
         self.player_manager.team_manager = self.team_manager  # Link TeamManager to PlayerManager
@@ -22,5 +26,4 @@ class LogicWrapper:
         )
         #self.search_manager: SearchManager = SearchManager(data.players, data.teams, data.tournaments)
         self.login_manager: LoginManager = LoginManager(data.players, data.roles, data.passwords)
-        from main.logic.leaderboardmanager import LeaderboardManager
         self.leaderboard_manager = LeaderboardManager(data)
